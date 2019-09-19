0709fb9c125882cdb1c9fd99e429157ebd6ec702
Pawel_93 <pawelszel1993@gmail.com>
2019-09-19 18:16:47 +0200

    branch without Builder

--- src/main/java/creativePatterns/_01_Builder/hause/Main.java
+++ src/main/java/creativePatterns/_01_Builder/hause/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Hause hause1 = new Hause("walls","floors","windows","rooms","doors", "garage");
         Hause hause2 = new Hause("walls","floors","windows");
+
+        System.out.println(hause1);
+        System.out.println(hause2);
     }
 }
