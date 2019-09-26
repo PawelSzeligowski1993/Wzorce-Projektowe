83a61e819f48e7af4f20d3a903b905339993caed
Pawel_93 <pawelszel1993@gmail.com>
2019-09-26 18:33:06 +0200

    branch with Factory 1)

--- src/main/java/creativePatterns/_02_Factory/strategyGame/Main.java
+++ src/main/java/creativePatterns/_02_Factory/strategyGame/Main.java
@@ -7,9 +7,11 @@ import creativePatterns._02_Factory.strategyGame.units.UnitType;
 
 public class Main {
     public static void main(String[] args) {
+
         Factory factory = new UnitFactory();
 
         Unit tank = factory.createUnit(UnitType.TANK);
         Unit infantryman = factory.createUnit(UnitType.RIFLEMAN);
+
     }
 }
